7cbce81b74b495cd46695d7bd64f6eacddba4dfd
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-04-22 09:04:29 +0300

    Update Valis.rb

--- Valis.rb
+++ Valis.rb
@@ -1,3 +1,3 @@
 1000000000000000000000.times do
-puts "1)Valis trilogy 2)Fall or dodge in hell 3) The three body problem trilogy"
+puts "(E)!= (mcmod2)!≈ (mcmod1)!=>lnE!=ln(mcmod2)!=lnE!≈ln(mcmod1)!"
 end
